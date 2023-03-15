67fa5e38fd90a4e723cad1f30c2cce27323bf113
Huzefa Mohammed Hussain <huzefamhd@gmail.com>
2023-03-15 12:55:42 +0400

    modal changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,46 @@
 
 //page change
 function goto($link) {
-
     window.location.href = "./" + $link + ".html";
-
-
 }
+
+//OPENING LOAN DETAILS
+
+function openModal(btnInfo) {
+    // console.log($(btnInfo).attr('class'));
+    //getting button class and using split in case the button has other classes
+    var buttonClass = $(btnInfo).attr('class').split(" ")[0];
+    $modal =
+        $('#' + buttonClass + "-modal").addClass("modal-animation-two").removeClass("out");
+    $('body').addClass('modal-active');
+}
+// $('.loan-details').click(function () {
+//     //getting button class and using split in case the button has other classes
+//     var buttonClass = $(this).attr('class').split(" ")[0];
+//     $modal =
+//         $('#' + buttonClass + "-modal").addClass("modal-animation-two").removeClass("out");
+//     $('body').addClass('modal-active');
+// })
+
+$('.close-modal-btn').click(function () {
+    //getting parent modal id to close it
+    var modalId = $(this).parent().parent().parent().attr("id");
+    $("#" + modalId).addClass('out');
+    $('body').removeClass('modal-active');
+});
+
+//icon animation
+$(document).ready(function () {
+    $('.animatedMenuIcon').click(function () {
+        $(this).toggleClass('open');
+        console.log($(".sidebar"))
+        if ($(this).hasClass('open')) {
+            $(".sidebar")[0].style.width = "100px";
+            $(".sidebar")[0].style.left = "0";
+        } else {
+            $(".sidebar")[0].style.width = "0px";
+            $(".sidebar")[0].style.left = "-100px";
+        }
+
+    });
+});
